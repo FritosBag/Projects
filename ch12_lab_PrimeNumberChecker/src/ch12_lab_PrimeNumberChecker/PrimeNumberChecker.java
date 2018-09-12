230acb78bdca61b95fc406fad7c045c5ba8512b4
FritosBag <37279302+FritosBag@users.noreply.github.com>
2018-09-12 14:26:55 -0500

    Add files via upload

--- ch12_lab_PrimeNumberChecker/src/ch12_lab_PrimeNumberChecker/PrimeNumberChecker.java
+++ ch12_lab_PrimeNumberChecker/src/ch12_lab_PrimeNumberChecker/PrimeNumberChecker.java
@@ -1,5 +1,6 @@
 package ch12_lab_PrimeNumberChecker;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -51,20 +52,44 @@ public class PrimeNumberChecker
 			}
 			while (errorCheck = false);
 			
-			//arrayList
+			ArrayList<Integer> primeArrayList = new ArrayList<Integer>(primeNumber);
+			ArrayList<Integer> factorArrayList = new ArrayList<Integer>(primeNumber);
 			
-			//for loop to check if number entered is prime
 			//primeNumber = index of the array
-			for (int i = 0; i < primeNumber; i++)
+			for (int i = 1; i <= primeNumber; i++)
 			{
-				
+				if (primeNumber % i == 0)
+				{
+					primeArrayList.add(i);
+				}
 			}
 			
-			//else print factors of non-prime number
+			for(int i = 1; i <= primeNumber; i++)
+			{
+				factorArrayList.add(i);
+			}
+			
+			if (primeArrayList.size() == 2)
+			{
+				System.out.println(primeNumber + " is a prime number.");
+			}
+			else
+			{
+				System.out.println(primeNumber + " is NOT a prime number.");
+				for (int i : factorArrayList)
+				{
+					System.out.print(i + ", ");
+				}
+				//put in method
+				
+				System.out.println("It has " + factorArrayList.size() + " factors: " + factorArrayList);
+
+			}
 			
 			System.out.println();
 			System.out.print("Try again? (y/n): ");
 			programEnd = sc.next();
+			System.out.println();
 		}
 		while (programEnd.equalsIgnoreCase("y"));
 		System.out.println();
